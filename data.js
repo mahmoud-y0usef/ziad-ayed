88031a64639b403147634d794a229dba444cfc7e
mahmoud-y0usef <mahmoudyousef59@outlook.com>
2023-05-16 01:45:33 +0300

    update

---
+++ data.js
@@ -0,0 +1,74 @@
+// about
+let home = {
+    name_logo: "Ziad",
+}
+let photo_slide = {
+        img1:"img1.jpg",
+        img2:"img2.jpg"
+};
+
+let products = {
+    photosession:"lorem ispum doler lorem ispum doler",
+    videoghrapher:"lorem ispum doler lorem ispum doler",
+    info:"lorem ispum doler lorem ispum lorem"
+}
+
+let cover = "cover.jpg"
+let numberOfWork = 100
+let testimony = [
+    {
+        descrption:"lorem ispum doler lorem ispum",
+        img:"person_1.jpg",
+        name:"Mahmoud Yousef",
+        jop:"backend developer"
+    },
+    {
+        descrption:"lorem ispum doler lorem ispum",
+        img:"person_1.jpg",
+        name:"Mahmoud Yousef",
+        jop:"backend developer"
+    },
+    {
+        descrption:"lorem ispum doler lorem ispum",
+        img:"person_1.jpg",
+        name:"Mahmoud Yousef",
+        jop:"backend developer"
+    },
+]
+
+// footer
+let footer = {
+    address:"tanta , Algharbia",
+    phone:"+201097537148",
+    email:"Zooz.ayed22@gmail.com",
+    aboutUs:"lorem ispum doler lorem ispum",
+    facebook:"https://www.facebook.com/Zyad.Ayed.Photoghraphy",
+    instgram:"https://www.instagram.com/zyad_ayedphotoghraphy"
+}
+
+// work
+let works = [
+    {
+        img:"work/session1.jpg",
+        type:"photo session",
+        date:"20/2/2023",
+        name:"session",
+        descrption:"lorem ispum doler lorem ispum doler",
+        view:"https://www.facebook.com/Zyad.Ayed.Photoghraphy/posts/581136280798266"
+    },
+    {
+        img:"work/session2.jpg",
+        date:"20/2/2023",
+        name:"session",
+        descrption:"lorem ispum doler lorem ispum doler",
+        view:"https://www.facebook.com/Zyad.Ayed.Photoghraphy/posts/pfbid0Js1XV4yV16skBpXdKbPgt1vxmksCLWBRRc9WdZtqMZGNyKvohhNXs5iNLJro4Dwql"
+    },
+    {
+        img:"work/session3.jpg",
+        type:"photo session",
+        date:"20/2/2023",
+        name:"session",
+        descrption:"lorem ispum doler lorem ispum doler",
+        view:"https://www.facebook.com/Zyad.Ayed.Photoghraphy/posts/pfbid0MpuY24vkFkoCseu5Ve7gfDSre8zGFR594gXBoZsVBaNMsTZ9TiszW9uL54kgTh5gl"
+    },
+]
